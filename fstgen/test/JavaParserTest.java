3d3ee55201823e7c0471c3913233ffb430f01460
tziadi <ziaditewfik@gmail.com>
2012-10-11 17:32:11 +0200

    test the new grammar

--- fstgen/test/JavaParserTest.java
+++ fstgen/test/JavaParserTest.java
@@ -6,12 +6,15 @@ import org.junit.Test;
 import tmp.generated_java15.Java15Parser;
 import cide.gparser.OffsetCharStream;
 import cide.gparser.ParseException;
+import de.ovgu.cide.fstgen.ast.FSTNonTerminal;
 
 public class JavaParserTest {
 	@Test
-	public void runParser() throws FileNotFoundException, ParseException {
-		Java15Parser p = new Java15Parser(new OffsetCharStream( new FileInputStream("test/Test.java")));
+	public void runParser() throws FileNotFoundException, ParseException, tmp.generated_java15java15_withDeepMethods.ParseException {
+		tmp.generated_java15java15_withDeepMethods.Java15Parser p = new tmp.generated_java15java15_withDeepMethods.Java15Parser(new FileInputStream("test/Test.java"));
 		p.CompilationUnit(false);
 		System.out.println(p.getRoot().printFST(0));
+		tmp.generated_java15java15_withDeepMethods.SimplePrintVisitor s = new tmp.generated_java15java15_withDeepMethods.SimplePrintVisitor();
+	    s.visit((FSTNonTerminal) p.getRoot());
 	}
 }
